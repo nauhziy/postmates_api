00bd10c57f8451078c863fd78f43d1f0b174e12f
YiZhuanFoong <yizhuanfoong@gmail.com>
2020-08-25 22:40:46 +0800

    added config folder

---
+++ config/development.py
@@ -0,0 +1,4 @@
+# Define the database - we are working with
+# SQLite for this example
+SQLALCHEMY_DATABASE_URI = 'postgresql://yizhuan@localhost:5432/postmates'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
